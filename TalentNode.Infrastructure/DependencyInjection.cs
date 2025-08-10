6646c96bc820a1d3dbb70d8ad6c6f235e0c58d1d
KumarShiv4321 <thakurshiv896@gmail.com>
2025-08-10 16:10:03 +0530

    connection string

--- TalentNode.Infrastructure/DependencyInjection.cs
+++ TalentNode.Infrastructure/DependencyInjection.cs
@@ -15,7 +15,7 @@ namespace TalentNode.Infrastructure
     {
         public static IServiceCollection AddInfrastuctureDI(this IServiceCollection services)
         {
-            services.AddDbContext<TalentNodeDbContext>(options => { options.UseSqlServer("Server =DESKTOP-M4HOJNG;Database=TalentNode;Trusted_Connection=true; Encrypt=true; TrustServerCertificate=true;"); });
+            services.AddDbContext<TalentNodeDbContext>(options => { options.UseSqlServer("Server =SHIV\\SQLEXPRESS_2022;Database=TalentNode;Trusted_Connection=true; Encrypt=true; TrustServerCertificate=true;"); });
 
             services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             return services;
