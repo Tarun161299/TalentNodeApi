4c03be61c32094843e73a9da57498b4f099b6e0a
KumarShiv4321 <thakurshiv896@gmail.com>
2025-08-17 16:09:13 +0530

    changes

---
+++ TalentNode.Domain/Models/Skills.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentNode.Domain.Models
+{
+    public class Skills
+    {
+        public int SkillID { get; set; }
+        public string SkillName { get; set; }
+    }
+}
