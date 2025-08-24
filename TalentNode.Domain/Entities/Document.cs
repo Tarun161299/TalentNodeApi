3e82bc900053959bd1e12d0178d883a6f36e5ac7
Tarun Singh <tarunsingh161299@gmail.com>
2025-08-24 00:12:21 -0700

    changes

--- TalentNode.Domain/Entities/Document.cs
+++ TalentNode.Domain/Entities/Document.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,21 +10,37 @@ namespace TalentNode.Domain.Entities
 {
     public class Document
     {
+        [Key]
         public int DocumentID { get; set; }      // Primary Key
+
+        [Required]
         public int EmployeeID { get; set; }      // Foreign Key (link to Employee table)
 
+        [Required, MaxLength(200)]
         public string DocName { get; set; }      // e.g. "Resume", "ID Proof"
-        public string FileName { get; set; }     // Actual saved file name
-        public string FilePath { get; set; }     // Relative path (e.g. "/Resumes/file.pdf")
+
+        [Required, MaxLength(200)]
+        public string FileName { get; set; }     // Original file name
+
+        [Required, MaxLength(50)]
         public string FileType { get; set; }     // pdf, docx, jpg, etc.
-        public string Link { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")]    // Use NVARCHAR(MAX) in SQL Server
+        public string FileContentBase64 { get; set; }  // Store file as Base64 string
+
+        [MaxLength(500)]
+        public string Link { get; set; }         // Optional link
+
         public DateTime UploadDate { get; set; } = DateTime.Now;
         public bool IsRemoved { get; set; } = false;
 
+        [MaxLength(100)]
         public string CreatedBy { get; set; }
+
+        [MaxLength(100)]
         public string UpdatedBy { get; set; }
 
-        // Navigation Property (optional, if you have Employee model)
-       
+        // Optional navigation property
+        // public Employee Employee { get; set; }
     }
 }
