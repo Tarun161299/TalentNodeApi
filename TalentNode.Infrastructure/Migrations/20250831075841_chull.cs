871d589400210647226c5420404074470f8aad62
KumarShiv4321 <thakurshiv896@gmail.com>
2025-08-31 16:27:13 +0530

    updatedb

---
+++ TalentNode.Infrastructure/Migrations/20250831075841_chull.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalentNode.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class chull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeID",
+                table: "Document");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeID",
+                table: "Document",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
