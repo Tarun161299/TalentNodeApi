facbb62bd625530981ef9e35388f1bf25ee673cc
KumarShiv4321 <thakurshiv896@gmail.com>
2025-10-04 17:36:46 +0530

    PersonalInfo

--- TalentNode.Application/command/UserProfileCommand.cs
+++ TalentNode.Application/command/UserProfileCommand.cs
@@ -1,14 +1,21 @@
-﻿//using MediatR;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using TalentNode.Domain.interfaces;
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentNode.Domain.interfaces;
+using TalentNode.Domain.Models;
 
-//namespace TalentNode.Application.command
-//{
-//    public class UserProfileCommand(SignupInterface SignupInterface) : IRequestHandler<SignupCommand, int>
-//    {
-//    }
-//}
+namespace TalentNode.Application.command
+{
+    public record UserProfileCommand(TalentNode.Domain.Models.UserProfileModel userProfile) : IRequest<int>;
+    public class UserProfileCommandHandler(UserProfileInterface userProfileInterface) : IRequestHandler<UserProfileCommand, int>
+    {
+        public async Task<int> Handle(UserProfileCommand request, CancellationToken cancellationToken)
+        {
+            return await userProfileInterface.Employee(request.userProfile);
+        }
+
+    }
+}
---
+++ TalentNode.Infrastructure/Repositories/UserProfileRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentNode.Application.command;
+using TalentNode.Domain.interfaces;
+using TalentNode.Infrastructure.Data;
+
+namespace TalentNode.Infrastructure.Repositories
+{
+    public class UserProfileRepository(TalentNodeDbContext dbContext) : UserProfileInterface
+
+    {
+        public async Task<int> Employee(TalentNode.Domain.Models.UserProfileModel userProfile)
+        {
+
+            TalentNode.Domain.Entities.Employee Employee = new TalentNode.Domain.Entities.Employee();
+            Employee.FirstName = userProfile.FirstName;
+            Employee.LastName = userProfile.LastName;
+            Employee.Email = userProfile.Email;
+            Employee.Phone = userProfile.Phone;
+            Employee.WorkingLocation = userProfile.Address;
+            Employee.StateID = userProfile.StateId;
+            Employee.DistrictID = userProfile.Districtid;
+            Employee.CurrentPosition = userProfile.CurrentPosition;
+            Employee.CurrentSalary = userProfile.CurrentSallary;
+            Employee.ExpectedSalary = userProfile.ExpectedSallary;
+
+            
+
+            dbContext.Employee.Add(Employee);
+            
+            return dbContext.SaveChanges();
+        }
+    }
+}
