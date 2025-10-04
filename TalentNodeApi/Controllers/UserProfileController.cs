1664cd37e6afb9a579a312aa0883b5361fee8ac8
KumarShiv4321 <thakurshiv896@gmail.com>
2025-10-04 14:28:28 +0530

    changes

---
+++ TalentNodeApi/Controllers/UserProfileController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TalentNode.Application.command;
+using TalentNode.Domain.Models;
+
+namespace TalentNodeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController(ISender sender) : ControllerBase
+    {
+        [HttpPost("SaveUserProfile")]
+        public async Task<int> UserProfileAsync([FromBody] SignupDetailsModel user)
+        {
+            try
+            {
+                int result = await sender.Send(new UserProfileCommand(user));
+                return result;
+            }
+    }
+}
