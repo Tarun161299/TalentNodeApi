8df11ac8f4bbb815bffbde1ca92463150bd3b9c7
KumarShiv4321 <thakurshiv896@gmail.com>
2026-06-22 02:56:27 +0530

    changes

---
+++ TalentNode.Application/command/RecommendedJobCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentNode.Domain.interfaces;
+using TalentNode.Domain.Models;
+
+namespace TalentNode.Application.command
+{
+        //public record RecommendedJobCommand(SearchForJobsModel searchModel) : IRequest<List<JobListForEmployee>>;
+
+        //public class RecommendedJobCommandHandler(IJobRepository IJobRepository) : IRequestHandler<RecommendedJobCommand, List<JobListForEmployee>>
+        //{
+        //    //public async Task<List<JobListForEmployee>> Handle(RecommendedJobCommand request, CancellationToken cancellationToken)
+        //    //{
+        //    //    return await IJobRepository.RecommendedJob(request.searchModel);
+        //    //}
+        //}
+    }
+
+
+
+    
+
